5826574a67496ea55e821d2b41d4beb24a393c00
xBorox1 <M.Borowski282@gmail.com>
2020-08-02 17:05:33 +0200

    Qubo_helper ended

--- src/qubo_helper.py
+++ src/qubo_helper.py
@@ -1,19 +1,22 @@
 from itertools import product
 
-# Simple class helping creating qubo dict.
+# Simple class that helps creating qubo dict for DWave solvers.
 class Qubo:
     def __init__(self):
         self.dict = dict()
-        #self.satisfied_energy = 0
 
+    # Creates new field in dict.
     def create_field(self, field):
         self.dict[field] = 0
 
+    # Creates new field in dict if it doesn't exist.
     def create_not_exist_field(self, field):
         if field in self.dict:
             return
         self.create_field(field)
 
+    # Adds constraint to qubo that exactly one of given variables should be equal to 1.
+    # Const parameter defines 'weight' of that constraint.
     def add_only_one_constraint(self, variables, const):
         for var in variables:
             self.create_not_exist_field((var, var))
@@ -21,25 +24,20 @@ class Qubo:
         for field in product(variables, variables):
             self.create_not_exist_field(field)
             self.dict[field] += const
-        #self.satisfied_energy -= const
 
-    def add_and_gate(self, x, y, z, const):
-        self.add((x, y), const)
-        self.add((x, z), (-2) * const)
-        self.add((y, z), (-2) * const)
-        self.add((z, z), 3 * const)
-
+    # Adds field to dict with given value.
     def add(self, field, value):
         self.create_not_exist_field(field)
         self.dict[field] += value
 
+    # Merges qubo qith another qubo. Consts parameters define 'weight' of each qubo.
     def merge_with(self, qubo, const1, const2):
         for field in self.dict:
             self.dict[field] *= const1
         for field in qubo.dict:
             self.create_not_exist_field(field)
             self.dict[field] += qubo.dict[field] * const2
-        #self.satisfied_energy += qubo.satisfied_energy
 
+    # Returns qubos dict which can be used in communication with DWave.
     def get_dict(self):
         return self.dict
