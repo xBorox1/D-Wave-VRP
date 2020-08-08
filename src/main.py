5ccadb591615c2f7e0d56b6ad787ab7e08e1b647
xBorox1 <M.Borowski282@gmail.com>
2020-08-08 15:31:24 +0200

    Input file

--- src/main.py
+++ src/main.py
@@ -12,22 +12,14 @@ if __name__ == '__main__':
     for t in ['example1', 'example2', 'example3']:
         print("Test : ", t)
 
-        TEST = '../tests/' + t + '.test'
-        test = read_test(TEST)
-
-        # Problem parameters
-        sources = test['sources']
-        costs = test['costs']
-        capacities = test['capacities']
-        dests = test['dests']
-        weigths = test['weights']
+        TEST = '../tests/vrp/' + t + '.test'
+        problem = read_test(TEST, capacity = False)
 
         only_one_const = 10000000.
         order_const = 1.
 
-        problem = VRPProblem(sources, costs, capacities, dests, weigths)
-        #solver = SolutionPartitioningSolver(problem, DBScanSolver(problem, anti_noiser = False))
-        solver = FullQuboSolver(problem)
+        solver = SolutionPartitioningSolver(problem, DBScanSolver(problem, anti_noiser = False))
+        #solver = FullQuboSolver(problem)
         #solver = AveragePartitionSolver(problem, limit_radius = 1)
 
         result = solver.solve(only_one_const, order_const, solver_type = 'cpu')
